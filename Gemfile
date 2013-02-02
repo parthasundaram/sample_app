a7ca8217ddc0a88b60aa5e4bb6c750b78a990a64
Partha Sundaram <parthasundaram@gmail.com>
2013-02-01 22:45:29 -0800

    basic user model

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.9'
 gem 'bootstrap-sass', '2.1'
+gem 'bcrypt-ruby', '3.0.1'
 
 group :production do	
 	gem 'pg', '0.12.2'
@@ -14,7 +15,9 @@ group :development, :test do
 	gem 'guard-rspec', '1.2.1'
 	gem 'guard-spork', '1.2.0'
 	gem 'spork', '0.9.2'
+	gem 'pg', '0.12.2'
 	gem 'sqlite3', '1.3.5'
+	gem 'annotate', '2.5.0'
 end
 
 # Gems used only for assets and not required
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,11 +4,18 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }    
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
+    let(:heading) {'Sample App'}
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Home' }
   end
 
